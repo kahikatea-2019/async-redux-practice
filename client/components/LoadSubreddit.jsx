40a685f7f958c73f0475cec9124909c24ca66743
Paul Renwick <prenwick@gmail.com>
2019-04-24 11:08:23 +1200

    Loading different subreddits

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -2,13 +2,29 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { fetchPosts } from '../actions'
 
-const LoadSubreddit = ({ children, dispatch }) => (
-  <div>
-    <button onClick={() => dispatch(fetchPosts('newzealand'))}>
-      Fetch Posts
-    </button>
-    {children}
-  </div>
-)
+class LoadSubreddit extends React.Component {
+    state = {
+      subreddit: ''
+    }
+
+    handleChange = (e) => {
+      this.setState({
+        subreddit: e.target.value
+      })
+    }
+
+    render () {
+      const { children, dispatch } = this.props
+      return (
+        <div>
+          <input name="subreddit" onChange={this.handleChange} value={this.state.wombat} />
+          <button onClick={() => dispatch(fetchPosts(this.state.subreddit))}>
+          Fetch Posts
+          </button>
+          {children}
+        </div>
+      )
+    }
+}
 
 export default connect()(LoadSubreddit)
