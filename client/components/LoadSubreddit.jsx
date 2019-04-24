0fbbf5b144ef18f45c6b47d105b5b5abb5410fdc
Eve Te Uru Henare <evehenare1@gmail.com>
2019-04-24 14:54:24 +1200

    Search field searchs in LoadSubreddit component

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -2,13 +2,31 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { fetchPosts } from '../actions'
 
-const LoadSubreddit = ({ children, dispatch }) => (
-  <div>
-    <button onClick={() => dispatch(fetchPosts('newzealand'))}>
-      Fetch Posts
-    </button>
-    {children}
-  </div>
-)
+class LoadSubreddit extends React.Component {
+  state = {
+    subreddit: ''
+  }
+
+  changeHandler = event => {
+    this.setState({
+      subreddit: event.target.value
+    })
+  }
+
+  render () {
+    const { dispatch, children } = this.props
+    const { subreddit } = this.state
+
+    return (
+      <div>
+        <input type='text' placeholder="type your countyyy here" onChange={this.changeHandler}></input>
+        <button onClick={() => dispatch(fetchPosts(subreddit))}>
+        Fetch Posts
+        </button>
+        {children}
+      </div>
+    )
+  }
+}
 
 export default connect()(LoadSubreddit)
