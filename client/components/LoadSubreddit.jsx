49108a69713097bb056b9c602193399c9dfebc1a
Anandani Mala Prakash <anandaniprakash@icloud.com>
2019-04-24 12:39:03 +1200

    added content to loadsubreddit

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -11,4 +11,32 @@ const LoadSubreddit = ({ children, dispatch }) => (
   </div>
 )
 
+
+handleChange = (e) => {
+  this.setState({
+    subreddit: e.target.value
+  })
+}
+
+// handleSave = () => {
+//   this.props.dispatch(saveWombat(this.state.wombat))
+//   this.setState({
+//     wombat: ''
+//   })
+// }
+
+render () {
+    const { children, dispatch} = this.props
+  return (
+    <React.Fragment>
+      <input name="subreddit" onChange={this.handleChange} value={this.state.wombat} />
+      <button onClick={() => dispatch(fetchPosts(this.state.subreddit))}>
+      Fetch Posts
+      </button>
+      {children}
+    </React.Fragment>
+  )
+}
+
+
 export default connect()(LoadSubreddit)
