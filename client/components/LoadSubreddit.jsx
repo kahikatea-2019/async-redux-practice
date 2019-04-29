401839d581751cd83cc6d97f54f7e5525add726e
Bradley Wheeler <bradley.wh@outlook.com>
2019-04-29 16:52:56 +1200

    can search for subreddits

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -2,13 +2,30 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { fetchPosts } from '../actions'
 
-const LoadSubreddit = ({ children, dispatch }) => (
-  <div>
-    <button onClick={() => dispatch(fetchPosts('newzealand'))}>
-      Fetch Posts
-    </button>
-    {children}
-  </div>
-)
+class LoadSubreddit extends React.Component {
+  state = {
+    subreddit: ''
+  }
+
+  changeHandler = e => {
+    this.setState({
+      subreddit: e.target.value
+    })
+  }
+
+  render () {
+    const { dispatch, children } = this.props
+    const { subreddit } = this.state
+    return (
+      <div>
+        <input type="text" placeholder = "Subreddit" onChange={this.changeHandler} />
+        <button onClick={() => dispatch(fetchPosts(subreddit))}>
+        Fetch Posts
+        </button>
+        {children}
+      </div>
+    )
+  }
+}
 
 export default connect()(LoadSubreddit)
