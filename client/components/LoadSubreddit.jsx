e3072e7393edf788fd0d0f8ef203e332e683604f
MurdocAV <azaan.virk1@gmail.com>
2019-05-01 22:29:38 +1200

    Coming @ you live from the staples centre

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { fetchPosts } from '../actions'
+import {connect} from 'react-redux'
+import {fetchPosts} from '../actions'
 
-const LoadSubreddit = ({ children, dispatch }) => (
-  <div>
-    <button onClick={() => dispatch(fetchPosts('newzealand'))}>
-      Fetch Posts
-    </button>
-    {children}
-  </div>
-)
+class LoadSubreddit extends React.Component {
+  state = {
+    subreddit: 'newzealand'
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  
+  render () {
+    const {children, dispatch} = this.props
+    return (
+      <div>
+        <p>reddit.com/r/{this.state.subreddit}</p>
+        <form>
+          <label>
+          <input onChange={this.handleChange} placeholder={this.state.subreddit} type="text" name="subreddit" />
+          </label>
+          <input onClick={() => dispatch(fetchPosts(this.state.subreddit))} type="submit" value="Fetch Posts" />
+        </form>
+        <br/>
+        {children}
+      </div>
+    )
+  }
+}
 
 export default connect()(LoadSubreddit)
--- client/components/Post.jsx
+++ client/components/Post.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
-const Post = ({ title }) => (
-  <div>{title}</div>
+const Post = ({title, summary, date}) => (
+  <div className="post">
+    <p><strong>Title: </strong>{title}</p>
+    <p><strong>Date: </strong>{date}</p>
+    <p><strong>Summary: </strong>{summary}</p>
+  </div>
 )
 
 export default Post
